94b7802bd6cfe7b799f127bedcd1d253c3a56692
Andy Duplain <trojanfoe@gmail.com>
2024-10-26 19:46:30 +0100

    Also manage .zprofile.

--- install.sh
+++ install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-set -x
 
 thisdir=$(realpath $(dirname $0))
+sys=$(uname -s)
 
-for d in nvim kitty
-do
-    dest=~/.config/$d
-    if [[ -d $dest ]]; then
-        rm -rf ~/.config/$d
-    fi
-    ln -sf $thisdir/config/$d $dest
+config_dirs="nvim"
+if [[ $sys = Darwin ]]; then
+  config_dirs="${config_dirs} kitty"
+
+fi
+for d in $config_dirs; do
+  dest=~/.config/$d
+  if ! test -h $dest; then
+    rm -rf $dest
+  fi
+  ln -sf $thisdir/config/$d $dest
 done
 
+ln -sf $thisdir/zprofile ~/.zprofile
 ln -sf $thisdir/zshrc ~/.zshrc
--- zshrc
+++ zshrc
@@ -1,6 +1,2 @@
 eval "$(starship init zsh)"
 
-export MANPATH="/usr/local/man:$MANPATH"
-export EDITOR=zed
-
-alias lg=lazygit
