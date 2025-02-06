3d44bf4495277d03f93c11deb08e650cd6025c22
Andy Duplain <trojanfoe@gmail.com>
2025-02-06 16:10:34 +0000

    More zed settings.

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sys=$(uname -s)
 mkdir $HOME/.config 2>/dev/null
 
 # Replace the entire directort
-config_dirs="nvim ghostty"
+config_dirs="nvim zed"
 for d in $config_dirs; do
   dest=~/.config/$d
   if test -d $dest; then
@@ -14,12 +14,6 @@ for d in $config_dirs; do
   fi
   ln -sf $thisdir/config/$d $dest
 done
-
-# Replace the specified files in a directory
-if [ "$sys" = "Darwin" ]; then
-    mkdir -p ~/.config/zed
-    ln -sf $thisdir/config/zed/settings.json ~/.config/zed
-fi
-
+s
 ln -sf $thisdir/zprofile ~/.zprofile
 ln -sf $thisdir/zshrc ~/.zshrc
